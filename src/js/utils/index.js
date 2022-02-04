f516b4e730cef905cc69b486be6746c41a522cc3
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-02-04 17:05:30 -0300

    Update variables export

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -1,14 +1,4 @@
-import dictionary from '../dictionary'
-export const $ = param => document.querySelector(param)
-export const $$ = param => document.querySelectorAll(param)
+import getWordOfTheDay from './getWordOfTheDay'
+import { $, $$ } from './documentSelectors.js'
 
-export function getWordOfTheDay() {
-  const now = new Date()
-  const start = new Date(2022, 0, 0)
-  const diff = Number(now) - Number(start)
-  let day = Math.floor(diff / (1000 * 60 * 60 * 24))
-  while (day > dictionary.length) {
-    day -= dictionary.length
-  }
-  return dictionary[day]
-}
+export default { getWordOfTheDay, $, $$ }
