f828478677d89806b593c2633dad8e59a3169b04
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-02-04 14:36:40 -0300

    Create word of the day getter

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -1,2 +1,14 @@
+import dictionary from '../dictionary'
 export const $ = param => document.querySelector(param)
 export const $$ = param => document.querySelectorAll(param)
+
+export function getWordOfTheDay() {
+  const now = new Date()
+  const start = new Date(2022, 0, 0)
+  const diff = Number(now) - Number(start)
+  let day = Math.floor(diff / (1000 * 60 * 60 * 24))
+  while (day > dictionary.length) {
+    day -= dictionary.length
+  }
+  return dictionary[day]
+}
