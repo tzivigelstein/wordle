6c31d2a4bfdc0208c7e40e85c2708367d24e44dc
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-02-11 11:27:53 -0300

    Handle timeout removing

--- src/js/Alert.js
+++ src/js/Alert.js
@@ -1,30 +1,50 @@
 export default class Alert {
-  constructor() {}
+  constructor() {
+    this.timeoutList = []
+  }
 
   triggerAlert(props) {
+    if (this.timeoutList.length > 0) {
+      console.log(this.timeoutList)
+      this.timeoutList.forEach(timeout => clearTimeout(timeout))
+      this.closeAlert()
+    }
+
     this.createAlert(props)
+
     if (props.type !== 'success') {
-      setTimeout(() => {
+      const timeout = setTimeout(() => {
         this.closeAlert()
       }, 2000)
+
+      this.timeoutList.push(timeout)
     }
   }
 
   closeAlert() {
-    document.querySelector('.alertContainer').remove()
+    const alert = document.querySelector('.alertContainer')
+    if (alert !== null) {
+      console.log({ alert })
+      alert.remove()
+    }
   }
 
   createAlert({ message, type }) {
+    const TYPES = {
+      success: 'alertSuccess',
+      error: 'alertError',
+      info: 'alertInfo'
+    }
+
     const alertContainer = document.createElement('div')
     alertContainer.classList.add('alertContainer')
-    alertContainer.classList.add(type === 'success' ? 'alertSuccess' : 'alertError')
+    alertContainer.classList.add(TYPES[type])
 
     const alertMessage = document.createElement('p')
     alertMessage.innerText = message
     alertMessage.classList.add('alertMessage')
 
     alertContainer.appendChild(alertMessage)
-    // alertContainer.appendChild(this.createCloseButton())
 
     document.body.appendChild(alertContainer)
   }
