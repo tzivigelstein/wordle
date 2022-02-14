85eae381b8dac1e56bb6351e0f0c5ed910321452
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-02-14 08:44:46 -0300

    Create dom interaction

---
+++ src/js/utils/dom.js
@@ -0,0 +1,12 @@
+export function createElement(props) {
+  const { elementType, innerText = '', classes = [] } = props
+
+  const element = document.createElement(elementType)
+  element.classList.add(...classes)
+  element.innerText = innerText
+
+  return element
+}
+
+export const $ = param => document.querySelector(param)
+export const $$ = param => document.querySelectorAll(param)
