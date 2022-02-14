289b012e4532343c3b0d4bfc538b106211cf2b1a
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-02-14 08:47:57 -0300

    Update method

--- src/js/utils/getRepeatedLettersIndexes.js
+++ src/js/utils/getRepeatedLettersIndexes.js
@@ -1,9 +1,9 @@
 export default function getRepeatedLettersIndexes({ firstWord, secondWord }) {
   const indexes = []
 
-  for (let i = 0; i < firstWord.length; i++) {
+  for (let i in firstWord) {
     if (firstWord[i] === secondWord[i]) {
-      indexes.push(i)
+      indexes.push(Number(i))
     }
   }
 
