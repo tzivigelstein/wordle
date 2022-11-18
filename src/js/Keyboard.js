882d6a7db223293b89eb62194a182305222b4029
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-11-18 15:57:43 -0300

    add desktop keyboard support

--- src/js/Keyboard.js
+++ src/js/Keyboard.js
@@ -1,74 +1,90 @@
-import { deleteButtonSVG } from './deleteButtonSVG.js'
-import utils from './utils/index.js'
-import keyboardLetters from './keyboardLetters.js'
-import { $addClass } from './utils/dom.js'
+import { deleteButtonSVG } from "./deleteButtonSVG.js";
+import utils from "./utils/index.js";
+import keyboardLetters from "./keyboardLetters.js";
+import { $addClass } from "./utils/dom.js";
 
-const { $, $$ } = utils
+const { $, $$ } = utils;
 
 export default class Keyboard {
   constructor() {
-    this.keyboardElement = $('.keyboardContainer')
-    this.initKeyboard()
+    this.keyboardElement = $(".keyboardContainer");
+    this.initKeyboard();
+    this.initListener();
   }
 
   initKeyboard() {
     keyboardLetters.forEach((row, index) => {
-      const keyboardLettersFragment = document.createElement('div')
-      keyboardLettersFragment.classList.add('keyboardRow')
-      row.forEach(letter => {
-        keyboardLettersFragment.appendChild(createKeyboardLetter(letter))
-      })
+      const keyboardLettersFragment = document.createElement("div");
+      keyboardLettersFragment.classList.add("keyboardRow");
+      row.forEach((letter) => {
+        keyboardLettersFragment.appendChild(createKeyboardLetter(letter));
+      });
 
       if (index === 2) {
-        const deleteButton = createDeleteButton()
-        keyboardLettersFragment.appendChild(deleteButton)
+        const deleteButton = createDeleteButton();
+        keyboardLettersFragment.appendChild(deleteButton);
       }
 
-      this.keyboardElement.appendChild(keyboardLettersFragment)
-    })
+      this.keyboardElement.appendChild(keyboardLettersFragment);
+    });
 
     function createKeyboardLetter({ letter, id }) {
-      const letterButton = document.createElement('button')
-      letterButton.id = id
-      letterButton.innerText = letter
-      letterButton.classList.add('keyboardButton')
+      const letterButton = document.createElement("button");
+      letterButton.id = id;
+      letterButton.name = letter;
+      letterButton.innerText = letter;
+      letterButton.classList.add("keyboardButton");
 
-      return letterButton
+      return letterButton;
     }
 
     function createDeleteButton() {
-      const deleteButton = document.createElement('button')
-      deleteButton.id = 28
-      deleteButton.innerHTML = deleteButtonSVG
-      deleteButton.classList.add('keyboardButton')
+      const deleteButton = document.createElement("button");
+      deleteButton.id = 28;
+      deleteButton.innerHTML = deleteButtonSVG;
+      deleteButton.classList.add("keyboardButton");
 
-      return deleteButton
+      return deleteButton;
     }
   }
 
+  initListener() {
+    window.addEventListener("keydown", (e) => {
+      const key = e.key;
+      const letter = document.querySelector(`[name="${key}"]`);
+      if (letter) {
+        letter.click();
+      } else if (key === "Backspace") {
+        document.getElementById(28).click();
+      } else if (key === "Enter") {
+        document.getElementById(20).click();
+      }
+    });
+  }
+
   onClick(callback) {
-    const htmlButtons = $$('.keyboardButton')
-    htmlButtons.forEach(button => {
-      button.addEventListener('click', e => {
-        callback({ letter: e.target.innerHTML, id: e.target.id })
-      })
-    })
+    const htmlButtons = $$(".keyboardButton");
+    htmlButtons.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        callback({ letter: e.target.innerHTML, id: e.target.id });
+      });
+    });
   }
 
   updateLettersStatus(keys) {
-    const [correctLetters, almostCorrectLetters, wrongLetters] = keys
-    const letters = $$('.keyboardButton')
+    const [correctLetters, almostCorrectLetters, wrongLetters] = keys;
+    const letters = $$(".keyboardButton");
 
-    letters.forEach(letter => {
-      const { innerHTML } = letter
+    letters.forEach((letter) => {
+      const { innerHTML } = letter;
 
       if (correctLetters.includes(innerHTML)) {
-        $addClass(letter, ['correctKeyboardLetter'])
+        $addClass(letter, ["correctKeyboardLetter"]);
       } else if (almostCorrectLetters.includes(innerHTML)) {
-        $addClass(letter, ['almostCorrectKeyboardLetter'])
+        $addClass(letter, ["almostCorrectKeyboardLetter"]);
       } else if (wrongLetters.includes(innerHTML)) {
-        $addClass(letter, ['wrongKeyboardLetter'])
+        $addClass(letter, ["wrongKeyboardLetter"]);
       }
-    })
+    });
   }
 }
