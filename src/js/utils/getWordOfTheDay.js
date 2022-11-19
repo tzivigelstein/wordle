79f9336072b456a04a9841c4a7469c207766e814
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-11-19 11:00:24 -0300

    update function arguments

--- src/js/utils/getWordOfTheDay.js
+++ src/js/utils/getWordOfTheDay.js
@@ -1,12 +1,12 @@
-import dictionary from '../dictionary'
+import dictionary from "../dictionary";
 
-export default function getWordOfTheDay() {
-  const now = new Date()
-  const start = new Date(2022, 0, 0)
-  const diff = Number(now) - Number(start)
-  let day = Math.floor(diff / (1000 * 60 * 60 * 24))
+export default function getWordOfTheDay(date = new Date()) {
+  const now = date;
+  const start = new Date(2022, 0, 0);
+  const diff = Number(now) - Number(start);
+  let day = Math.floor(diff / (1000 * 60 * 60 * 24));
   while (day > dictionary.length) {
-    day -= dictionary.length
+    day -= dictionary.length;
   }
-  return dictionary[day]
+  return dictionary.at(day);
 }
