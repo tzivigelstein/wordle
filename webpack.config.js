8529c712e222b4111e1360dd6f55b8325b2b2142
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-11-19 18:59:47 -0300

    prettier fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import CopyWebpackPlugin from "copy-webpack-plugin";
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import CopyWebpackPlugin from 'copy-webpack-plugin'
 
-const { pathname: source } = new URL("src", import.meta.url);
-const { pathname: build } = new URL("build", import.meta.url);
+const { pathname: source } = new URL('src', import.meta.url)
+const { pathname: build } = new URL('build', import.meta.url)
 
 export default {
   devServer: {
@@ -11,10 +11,10 @@ export default {
     open: true,
     liveReload: true,
   },
-  entry: "./src/js/main.js",
+  entry: './src/js/main.js',
   output: {
     path: build,
-    filename: "main.js",
+    filename: 'main.js',
   },
   module: {
     rules: [
@@ -22,7 +22,7 @@ export default {
         test: /\.(js)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
         resolve: {
           fullySpecified: false,
@@ -30,18 +30,18 @@ export default {
       },
     ],
   },
-  mode: "production",
+  mode: 'production',
   plugins: [
-    new HtmlWebpackPlugin({ template: "src/index.html" }),
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
     new CopyWebpackPlugin({
       patterns: [
-        { from: "src/assets", to: "assets" },
-        { from: "src/styles/styles.css", to: "styles/styles.css" },
-        { from: "src/styles/game.css", to: "styles/game.css" },
-        { from: "src/styles/rules.css", to: "styles/rules.css" },
-        { from: "src/styles/stats.css", to: "styles/stats.css" },
-        { from: "src/styles/settings.css", to: "styles/settings.css" },
+        { from: 'src/assets', to: 'assets' },
+        { from: 'src/styles/styles.css', to: 'styles/styles.css' },
+        { from: 'src/styles/game.css', to: 'styles/game.css' },
+        { from: 'src/styles/rules.css', to: 'styles/rules.css' },
+        { from: 'src/styles/stats.css', to: 'styles/stats.css' },
+        { from: 'src/styles/settings.css', to: 'styles/settings.css' },
       ],
     }),
   ],
-};
+}
